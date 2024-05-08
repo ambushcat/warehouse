708fcc64f20fd8ea819f6211406b0654b88f4c6d
AmberCDH <kanarieamber@gmail.com>
2024-05-08 12:04:57 +0200

    Refactored the audio system

---
+++ Assets/Scripts/Services/DataStorageManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Services
+{
+    [CreateAssetMenu(fileName = "DataStorageManagerConfig", menuName = "ScriptableObjects/DataStorageManager", order = 1)]
+    public class DataStorageManager : ScriptableObject
+    {
+        public string volumeKey;
+        public float Volume
+        {
+            get
+            {
+                return PlayerPrefs.GetFloat(volumeKey, 1);
+            }
+            set
+            {
+                PlayerPrefs.SetFloat(volumeKey, value);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Settings/Settings.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Services;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Settings
+{
+    public class Settings : MonoBehaviour
+    {
+        public Slider slider;
+        public string mainMenuSceneName;
+        public ServiceLocator serviceLoactor;
+
+        private void Start()
+        {
+            slider.value = serviceLoactor.GetAudioManager().Volume;
+        }
+
+        public void SetVolume(float value)
+        {
+            serviceLoactor.GetAudioManager().Volume = value;
+        }
+
+        public void BackToMainMenu()
+        {
+            SceneManager.LoadScene(mainMenuSceneName);
+        }
+    }
+}
