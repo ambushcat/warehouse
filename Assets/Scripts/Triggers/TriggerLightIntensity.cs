f662c8f84e3b76663517b212dfa593fd1f8b649d
Tristan Schuring <tristandevelop@protonmail.com>
2024-05-20 18:00:49 +0200

    Fixed lightning

--- Assets/Scripts/Triggers/TriggerLightIntensity.cs
+++ Assets/Scripts/Triggers/TriggerLightIntensity.cs
@@ -1,14 +1,38 @@
+using System.Collections;
 using UnityEngine;
 
 public class TriggerLightIntensity : MonoBehaviour, ITriggerableObject
 {
     [SerializeField]
-    ReflectionProbe reflectionProbe;
+    Light directionalLight;
     [SerializeField]
-    float intensity;
+    float targetIntensity;
+    [SerializeField]
+    float duration = 1.0f; // Duration over which to change the intensity
+
+    private Coroutine intensityCoroutine;
 
     public void Trigger()
     {
-        reflectionProbe.intensity = intensity;
+        if (intensityCoroutine != null)
+        {
+            StopCoroutine(intensityCoroutine);
+        }
+        intensityCoroutine = StartCoroutine(ChangeIntensityOverTime(directionalLight.intensity, targetIntensity, duration));
+    }
+
+    private IEnumerator ChangeIntensityOverTime(float startIntensity, float endIntensity, float duration)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            directionalLight.intensity = Mathf.Lerp(startIntensity, endIntensity, elapsed / duration);
+            yield return null;
+        }
+
+        // Ensure the final value is set after the loop
+        directionalLight.intensity = endIntensity;
     }
 }
